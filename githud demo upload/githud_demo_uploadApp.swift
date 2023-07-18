b5bbcd0d52363fb7153fa3fbc05a09d9affa36d2
Mariam Alfeel <mariam.uae65@gmail.com>
2023-07-18 11:18:04 +0400

    Initial Commit

---
+++ githud demo upload/githud_demo_uploadApp.swift
@@ -0,0 +1,17 @@
+//
+//  githud_demo_uploadApp.swift
+//  githud demo upload
+//
+//  Created by Mariam Younis Alfeel on 18/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct githud_demo_uploadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ githud demo uploadUITests/githud_demo_uploadUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  githud_demo_uploadUITestsLaunchTests.swift
+//  githud demo uploadUITests
+//
+//  Created by Mariam Younis Alfeel on 18/07/2023.
+//
+
+import XCTest
+
+final class githud_demo_uploadUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
