2a0f67cf617b2a76ea65290951d8463a2488eb3a
Mariam Alfeel <mariam.uae65@gmail.com>
2023-07-18 11:34:41 +0400

    made new changes

--- githud demo upload/ContentView.swift
+++ githud demo upload/ContentView.swift
@@ -8,14 +8,18 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+var body: some View {
+    VStack {
+Image (systemName: "globe")
+imageScale(.large)
+     .foregroundColor (.accentColor )
+Text ("Hello, world!")
+Text ("This is great")
         }
-        .padding()
+    .multilineTextAlignment (.center)
+    .font (.system(size: 100))
+    .foregroundColor (.brown)
+    padding ()
     }
 }
 
